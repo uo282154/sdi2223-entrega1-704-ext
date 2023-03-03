d8629c85d69e40e9eb7eaf24c4055f7a7af91f95
Diego <garciaquiros.diego@gmail.com>
2023-03-03 15:32:53 +0100

    Initial commit

---
+++ src/test/java/com/uniovi/sdi/sdi2223entrega171/Sdi2223Entrega171ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi.sdi2223entrega171;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2223Entrega171ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
