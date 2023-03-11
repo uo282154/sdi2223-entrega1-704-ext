733895d7cf74b571b61173d0ac554af55be7b94b
Diego <garciaquiros.diego@gmail.com>
2023-03-11 12:08:00 +0100

    Solve Pagination problem

--- src/main/java/com/uniovi/sdi/sdi2223entrega171/CustomConfiguration.java
+++ src/main/java/com/uniovi/sdi/sdi2223entrega171/CustomConfiguration.java
@@ -1,13 +1,21 @@
 package com.uniovi.sdi.sdi2223entrega171;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.List;
+import java.util.Locale;
 
+@Configuration
 public class CustomConfiguration implements WebMvcConfigurer {
 
     @Value("${spring.data.web.pageable.page-parameter}")
@@ -24,6 +32,7 @@ public class CustomConfiguration implements WebMvcConfigurer {
         resolver.setFallbackPageable(PageRequest.of(page, size));
         argumentResolvers.add(resolver);
     }
+
 
 
         //Faltarian interceptores y demás para la internacionalización
